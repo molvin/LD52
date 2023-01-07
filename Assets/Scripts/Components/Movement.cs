4e6ef87ed98855945848bf8c1d14013210bfebde
daniel-fors <daniel.lennart.fors@gmail.com>
2023-01-07 15:25:14 +0100

    movement

--- Assets/Scripts/Components/Movement.cs
+++ Assets/Scripts/Components/Movement.cs
@@ -6,39 +6,58 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Movement : MonoBehaviour
 {
+    [Header("Steering")]
+    public float Speed = 2.5f;
+
+    [Header("Steering")]
+    public float Acceleration = 16.0f;
+
+    [Header("Steering")]
+    public float AngularSpeed = 200.0f;
+
+    [Header("Steering")]
+    public float StoppingDistance = 0.5f;
+
     public bool DebugDraw = false;        
     private NavMeshAgent Agent;
+    private Selectable Selectable = null;
     private Color DebugColor;
 
     void Awake()
     {
         Agent = GetComponent<NavMeshAgent>();
+        Selectable = GetComponent<Selectable>();
+
         if (DebugDraw)
         {
             Random.InitState(GetHashCode());
             DebugColor = Random.ColorHSV();
+        }
+
+        if (Selectable == null)
+        {
+            Debug.LogError("No selectable!");
+            enabled = false;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (DebugDraw)
-        {
-            Vector3[] corners = Agent.path.corners;
-            Debug.Log(corners.Length);
-            if (corners.Length <= 1)
-            {
-                Debug.DrawLine(transform.position, Agent.destination, DebugColor, Time.deltaTime);
-            }
-            else
-            {
-                for (int i = 1; i < corners.Length; i++)
-                {
-                    Debug.DrawLine(corners[i - 1], corners[i], DebugColor, Time.deltaTime);
-                }
-            }
+        { // Update agent
+            Agent.speed            = Speed;
+            Agent.angularSpeed     = AngularSpeed;
+            Agent.acceleration     = Acceleration;
+            Agent.stoppingDistance = StoppingDistance;
         }
+
+        if (Selectable.TargetPosition.Dist2D(Agent.destination) >= StoppingDistance)
+        {
+            Agent.destination = Selectable.TargetPosition;
+        }
+
+        if (DebugDraw)
+            DrawDebug();
     }
 
     public void MoveTo(Vector3 Destination)
@@ -48,4 +67,19 @@ public class Movement : MonoBehaviour
 
     public Vector3 GetDestination() => Agent.destination;
 
+    private void DrawDebug()
+    {
+        Vector3[] corners = Agent.path.corners;
+        if (corners.Length <= 1)
+        {
+            Debug.DrawLine(transform.position, Agent.destination, DebugColor, Time.deltaTime);
+        }
+        else
+        {
+            for (int i = 1; i < corners.Length; i++)
+            {
+                Debug.DrawLine(corners[i - 1], corners[i], DebugColor, Time.deltaTime);
+            }
+        }
+    }
 }
