c9a5d8b0092650076375aa90e62c466107598e32
molvin <akerstrom.perry@gmail.com>
2023-01-08 17:29:23 +0100

    harvest

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -60,7 +60,8 @@ public class HealthBar : MonoBehaviour
     {
         if (health)
         {
-            Background.enabled = Fill.enabled = health.Current < health.Max;
+            //Background.enabled = Fill.enabled = health.Current < health.Max;
+            gameObject.SetActive(health.Current < health.Max);
             Fill.fillAmount = (health.Current / (float)health.Max);
         }
     }
