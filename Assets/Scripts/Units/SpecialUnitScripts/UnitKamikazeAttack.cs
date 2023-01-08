1605d138547db560d85b3032e046f24af35fd0e7
marinlord <morrismertner@gmail.com>
2023-01-08 12:56:43 +0100

    ling bling kaplow

--- Assets/Scripts/Units/SpecialUnitScripts/UnitKamikazeAttack.cs
+++ Assets/Scripts/Units/SpecialUnitScripts/UnitKamikazeAttack.cs
@@ -5,26 +5,43 @@ using UnityEngine;
 public class UnitKamikazeAttack : UnitAttack
 {
     public float ExplosionActivationRange, ExplosionRadius;
+    public bool Exploding;
 
     protected override void Attack(Entity Target)
     {
-        for(int i = 0; i<GameManager.Instance.playerUnits.Count; i++)
+        if (!Exploding)
         {
-            if(Vector2.Distance(Entity.gameObject.transform.position, Target.gameObject.transform.position) <= ExplosionRadius)
-            {
-                RaycastHit Hit;
-                if (Physics.Raycast(transform.position,(GameManager.Instance.playerUnits[i].gameObject.transform.position - Entity.gameObject.transform.position).normalized, out Hit, AttackDistance) && Hit.transform == GameManager.Instance.playerUnits[i].gameObject.transform)
-                {
-                    GameManager.Instance.playerUnits[i].GetComponent<UnitHealth>().TakeDamage(Damage);
-                }
-            }
+            StartCoroutine(GoKaploow(Target));
         }
+
+
 
         //Target.Get<UnitHealth>().TakeDamage(Damage);
 
         
-        Entity.Get<UnitHealth>().TakeDamage(100);
+        
 
 
     }
+     private IEnumerator GoKaploow(Entity Target)
+    {
+        Exploding = true;
+        Entity.Get<Movement>().enabled = false;
+        yield return new WaitForSeconds(1);
+        for (int i = 0; i < GameManager.Instance.playerUnits.Count; i++)
+        {
+            if (Vector2.Distance(Entity.gameObject.transform.position, Target.gameObject.transform.position) <= ExplosionRadius)
+            {
+                RaycastHit Hit;
+                if (Physics.Raycast(transform.position, (GameManager.Instance.playerUnits[i].gameObject.transform.position - Entity.gameObject.transform.position).normalized, out Hit, AttackDistance) && Hit.transform == GameManager.Instance.playerUnits[i].gameObject.transform)
+                {
+                    GameManager.Instance.playerUnits[i].GetComponent<UnitHealth>().TakeDamage(Damage);
+
+                }
+            }
+        }
+        Entity.Get<UnitHealth>().TakeDamage(500000000);
+        yield return 0;
+    }
+    
 }
