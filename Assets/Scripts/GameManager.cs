d2e9619048cd60818c58f0ebe4ea227ef4c54904
daniel-fors <daniel.lennart.fors@gmail.com>
2023-01-09 19:10:47 +0100

    level 9

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ using static UnityEngine.UI.Image;
 
 public class GameManager : MonoBehaviour
 {
-    public static int LevelCount = 8;
+    public static int LevelCount = 9;
     public enum State
     {
         Start,
